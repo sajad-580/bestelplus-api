5b842ba673093aa4af57ff8e9928e94d02c58950
Sajad Norouzi <sajad.nrz@yahoo.com>
2024-11-13 17:56:35 +0330

    fix cors error

---
+++ server.js
@@ -0,0 +1,16 @@
+const jsonServer = require("json-server");
+const cors = require("cors");
+const server = jsonServer.create();
+const router = jsonServer.router("server/db.json");
+const middlewares = jsonServer.defaults();
+
+server.use(cors()); // Enable CORS for all routes
+server.use(middlewares);
+server.use(router);
+
+const PORT = 80;
+const HOST = "0.0.0.0";
+
+server.listen(PORT, HOST, () => {
+  console.log(`JSON Server is running on http://${HOST}:${PORT}`);
+});
